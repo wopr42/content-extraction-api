6e4901fab586cded25e85a59272384adf0365966
John Gray <wopr42@gmail.com>
2017-11-04 02:34:25 -0500

    Convert to JSON API using Flask-RESTPlus
    Also include keywords and URL in response.

--- app.py
+++ app.py
@@ -1,17 +1,41 @@
 from flask import Flask
-from flask import request
+from flask_restplus import Api, Resource, fields
 from newspaper import Article
 
 app = Flask(__name__)
+api = Api(app)
 
-@app.route('/')
-def hello():
-    return 'Hello World!'
+model = api.model('Model', {
+    'keywords': fields.List(fields.String),
+    'title': fields.String,
+    'url': fields.String
+})
 
-@app.route('/article_extractions')
-def extract():
-    url = request.args.get('url', '')
-    article = Article(url)
-    article.download()
-    article.parse()
-    return article.title
+class ArticleExtraction(object):
+    def __init__(self, data):
+        self.keywords = data.get('keywords', [])
+        self.title = data['title']
+        self.url = data['url']
+
+        self.keywords.sort()
+
+@api.route('/article_extractions', methods=['POST'])
+class ArticleExtractions(Resource):
+    @api.expect(model)
+    @api.marshal_with(model, code=201)
+    def post(self):
+        url = api.payload['url']
+        article = Article(url)
+
+        article.download()
+        article.parse()
+        article.nlp()
+
+        return ArticleExtraction({
+            'keywords': article.keywords,
+            'title': article.title,
+            'url': url
+        }), 201
+
+if __name__ == '__main__':
+    app.run(debug=True)
