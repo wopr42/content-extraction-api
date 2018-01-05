2d586372929574743221497c340752e9a0a591a7
John Gray <wopr42@gmail.com>
2018-01-05 01:10:24 -0600

    Include summary, text, and image

--- app.py
+++ app.py
@@ -7,14 +7,20 @@ api = Api(app)
 
 model = api.model('Model', {
     'keywords': fields.List(fields.String),
+    'summary': fields.String,
+    'text': fields.String,
     'title': fields.String,
+    'top_image': fields.String,
     'url': fields.String
 })
 
 class ArticleExtraction(object):
     def __init__(self, data):
         self.keywords = data.get('keywords', [])
+        self.summary = data['summary']
+        self.text = data['text']
         self.title = data['title']
+        self.top_image = data['top_image']
         self.url = data['url']
 
         self.keywords.sort()
@@ -33,7 +39,10 @@ class ArticleExtractions(Resource):
 
         return ArticleExtraction({
             'keywords': article.keywords,
+            'summary': article.summary,
+            'text': article.text,
             'title': article.title,
+            'top_image': article.top_image,
             'url': url
         }), 201
 
